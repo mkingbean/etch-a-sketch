2aa20732a94735539b4c4e4e8a52ec66d5995e8b
matt <git@mking.io>
2025-11-12 19:29:49 +0000

    Create function to generate a grid of arbitrary size

--- sketch.js
+++ sketch.js
@@ -4,17 +4,19 @@ function paintSquare(e) {
     e.target.classList.add("painted");
 }
 
-let rows = [];
-let squares = [];
-for (let i = 0; i < 4; i++) {
-    rows[i] = document.createElement("div");
-    rows[i].classList.add("row");
-    container.appendChild(rows[i]);
-    squares[i] = [];
-    for (let j = 0; j < 4; j++) {
-        squares[i][j] = document.createElement("div");
-        squares[i][j].classList.add("square");
-        squares[i][j].addEventListener("mouseover", paintSquare);
-        rows[i].appendChild(squares[i][j]);
+function generateGrid(size) {
+    let rows = [];
+    let squares = [];
+    for (let i = 0; i < size; i++) {
+        rows[i] = document.createElement("div");
+        rows[i].classList.add("row");
+        container.appendChild(rows[i]);
+        squares[i] = [];
+        for (let j = 0; j < size; j++) {
+            squares[i][j] = document.createElement("div");
+            squares[i][j].classList.add("square");
+            squares[i][j].addEventListener("mouseover", paintSquare);
+            rows[i].appendChild(squares[i][j]);
+        }
     }
 }
